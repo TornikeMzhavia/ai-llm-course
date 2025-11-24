5b23abd577adf913c0c2ba2a3655482d48ff7b4b
TornikeMzhavia <tornike@bitfount.com>
2025-11-24 14:30:38 +0000

    added jupiter for second example

--- 6. ollama/start-2.py
+++ 6. ollama/start-2.py
@@ -44,13 +44,13 @@ res = ollama.generate(
 
 
 # Create a new model with modelfile
-modelfile = """
+system_prompt = """
 FROM llama3.2
 SYSTEM You are very smart assistant who knows everything about oceans. You are very succinct and informative.
 PARAMETER temperature 0.1
 """
 
-ollama.create(model="knowitall", modelfile=modelfile)
+ollama.create(model="knowitall", system=system_prompt)
 
 res = ollama.generate(model="knowitall", prompt="why is the ocean so salty?")
 print(res["response"])
